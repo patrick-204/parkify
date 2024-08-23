c77faec8dd77e404fed71c53750a760626076d7d
Mussie <musdzion@gmail.com>
2024-08-22 21:36:25 -0400

    add reservations table with seeds

---
+++ db/schema/04_reservations.sql
@@ -0,0 +1,11 @@
+-- Drop and recreate Users table (Example)
+
+DROP TABLE IF EXISTS reservations CASCADE;
+
+CREATE TABLE reservations (
+  id SERIAL PRIMARY KEY NOT NULL,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  parking_space_id INTEGER REFERENCES parking_spaces(id) ON DELETE CASCADE,
+  reservation_start TIMESTAMP,
+  reservation_end  TIMESTAMP
+);
