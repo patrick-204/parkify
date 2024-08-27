2cb5bda957387fbb2f188063a972277c59005389
Mussie <musdzion@gmail.com>
2024-08-27 07:31:13 -0400

    add parking route to homepage

--- server.js
+++ server.js
@@ -1,59 +1,69 @@
 // load .env data into process.env
-require('dotenv').config();
+require("dotenv").config();
+
 
 // Web server config
-const sassMiddleware = require('./lib/sass-middleware');
-const express = require('express');
-const morgan = require('morgan');
+const sassMiddleware = require("./lib/sass-middleware");
+const express = require("express");
+const morgan = require("morgan");
 
 const PORT = process.env.PORT || 8080;
 const app = express();
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(
-  '/styles',
+  "/styles",
   sassMiddleware({
-    source: __dirname + '/styles',
-    destination: __dirname + '/public/styles',
+    source: __dirname + "/styles",
+    destination: __dirname + "/public/styles",
     isSass: false, // false => scss, true => sass
   })
 );
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
-const userApiRoutes = require('./routes/users-api');
-const usersRoutes = require('./routes/users');
-const parkingSpacesRoutes = require ('./routes/parking_spaces_api')
-const vehiclesRoutes = require ('./routes/vehicles')
-const vehicleApiRoutes = require ('./routes/vehicles-api')
-const reservationsRoutes = require ('./routes/reservations_api')
-const paymentsRoutes = require ('./routes/payments_api')
+const userApiRoutes = require("./routes/users-api");
+const usersRoutes = require("./routes/users");
+const parkingSpacesRoutes = require("./routes/parking_spaces_api");
+const vehiclesRoutes = require("./routes/vehicles");
+const vehicleApiRoutes = require("./routes/vehicles-api");
+const reservationsRoutes = require("./routes/reservations_api");
+const paymentsRoutes = require("./routes/payments_api");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 // Note: Endpoints that return data (eg. JSON) usually start with `/api`
-app.use('/api/users', userApiRoutes);
-app.use('/users', usersRoutes);
-app.use('/api/vehicles', vehicleApiRoutes );
-app.use('/vehicles', vehiclesRoutes );
-app.use('/api/parking_spaces', parkingSpacesRoutes);
-app.use('/api/reservations', reservationsRoutes)
-app.use('/api/payments', paymentsRoutes)
+app.use("/api/users", userApiRoutes);
+app.use("/users", usersRoutes);
+app.use("/api/vehicles", vehicleApiRoutes);
+app.use("/vehicles", vehiclesRoutes);
+app.use("/api/parking_spaces", parkingSpacesRoutes);
+app.use("/api/reservations", reservationsRoutes);
+app.use("/api/payments", paymentsRoutes);
 // Note: mount other resources here, using the same pattern above
 
 // Home page
 // Warning: avoid creating more routes in this file!
 // Separate them into separate routes files (see above).
+app.get("/",(req, res) => {
 
-app.get('/', (req, res) => {
-  res.render('index');
+    const parkingSpacesQueries = require("./db/queries/parking_spaces");
+
+    // Fetch parking spaces
+    parkingSpacesQueries.getParkingSpaces()
+       .then(parking_spaces => {
+              res.render('index', {parking_spaces})
+       })
+
+
+
 });
 
 
