d62a4e1246aa092dd8fed90af6e33adc7dafb9fd
Mussie <musdzion@gmail.com>
2024-09-02 21:49:52 -0400

    modify schema for creating parking spaces

--- db/schema/02_parking_spaces.sql
+++ db/schema/02_parking_spaces.sql
@@ -3,6 +3,10 @@ DROP TABLE IF EXISTS parking_spaces CASCADE;
 CREATE TABLE parking_spaces (
     id SERIAL PRIMARY KEY NOT NULL,
     location VARCHAR(20) NOT NULL,
+    street_address VARCHAR(50) NOT NULL,
+    city VARCHAR(30) NOT NULL,
+    province VARCHAR(20) NOT NULL,
+    price DECIMAL(10, 2),
     isReserved BOOLEAN DEFAULT FALSE,
     owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE
 );
