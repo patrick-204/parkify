685e8df4544e922a5cc9326907f179ef57e82d15
Mussie <musdzion@gmail.com>
2024-09-02 22:06:35 -0400

    add route to server file

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const db = require('./db/connection'); // Import the PostgreSQL db
 const PORT = process.env.PORT || 8080;
 const app = express();
 
-// React implementation 
+// React implementation
 const path = require('path');
 app.use(express.static(path.join(__dirname, 'frontend/build')));
 
@@ -48,7 +48,7 @@ io.on('connection', (socket) => {
   });
 });
 
-app.set('view engine', 'ejs');
+// app.set('view engine', 'ejs');
 
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
@@ -74,7 +74,7 @@ app.use(
 
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
-const userApiRoutes = require('./routes/users-api');
+const userParkingInfoRoutes = require('./routes/userParkingInfoRoutes');
 const usersRoutes = require('./routes/users');
 const userRegistrationRoutes = require('./routes/user-registration');
 const userLoginRoutes = require('./routes/user-login');
@@ -88,8 +88,8 @@ const paymentsRoutes = require('./routes/checkout');
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 // Note: Endpoints that return data (eg. JSON) usually start with `/api`
-app.use('/api/users', userApiRoutes);
-app.use('/users', usersRoutes);
+
+app.use('/users', userParkingInfoRoutes);
 app.use('/users', userRegistrationRoutes);
 app.use('/login', userLoginRoutes);
 app.use('/logout', userLogoutRoutes);
@@ -101,11 +101,11 @@ app.use('/checkout', paymentsRoutes);
 // Note: mount other resources here, using the same pattern above
 
 
-// React Implementation 
+// React Implementation
 /* app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
 });
-*/ 
+*/
 
 
 app.get('/api/items', async (req, res) => {
@@ -138,4 +138,4 @@ app.get('/', (req, res) => {
 
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
-*/ 
+*/
